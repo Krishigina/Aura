449f377c3bcdda30f4ee756dfb86a57bc94bd416
krishigina <krishigina@gmail.com>
2026-04-08 10:25:00 +0300

    feat(mobile): improve api client and dependency wiring

--- mobile/shared/src/commonMain/kotlin/com/aura/core/di/AppModule.kt
+++ mobile/shared/src/commonMain/kotlin/com/aura/core/di/AppModule.kt
@@ -4,5 +4,5 @@ import com.aura.core.data.api.AuraApiClient
 import org.koin.dsl.module
 
 val appModule = module {
-    single { AuraApiClient("http://10.0.2.2:8000") }
+    single { AuraApiClient("http://10.0.2.2:3002") }
 }
