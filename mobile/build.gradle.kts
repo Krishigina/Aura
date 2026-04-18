1dea10908a6b852bd28fdd6c74a32be235dca918
krishigina <krishigina@gmail.com>
2026-04-18 10:05:00 +0300

    build(mobile): expand shared module and gradle tooling

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.compose") version "1.5.10" apply false
+    id("com.diffplug.spotless") version "6.25.0" apply false
     kotlin("multiplatform") version "1.9.20" apply false
     kotlin("plugin.serialization") version "1.9.20" apply false
 }
--- mobile/shared/build.gradle.kts
+++ mobile/shared/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("plugin.serialization")
     id("com.android.library")
     id("org.jetbrains.compose")
+    id("com.diffplug.spotless")
 }
 
 repositories { mavenCentral(); google() }
@@ -13,6 +14,21 @@ kotlin {
             kotlinOptions { jvmTarget = "17" }
         }
     }
+
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    targets
+        .withType(org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget::class.java)
+        .configureEach {
+            binaries {
+                framework {
+                    baseName = "shared"
+                    isStatic = true
+                }
+            }
+        }
     
     sourceSets {
         val commonMain by getting {
@@ -23,17 +39,40 @@ kotlin {
                 implementation("androidx.navigation:navigation-compose:2.7.6")
                 implementation("androidx.compose.material:material-icons-extended:1.5.4")
                 implementation("io.ktor:ktor-client-core:2.3.7")
-                implementation("io.ktor:ktor-client-okhttp:2.3.7")
                 implementation("io.ktor:ktor-client-content-negotiation:2.3.7")
                 implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.7")
                 implementation("io.insert-koin:koin-core:3.5.3")
+                implementation("androidx.lifecycle:lifecycle-viewmodel:2.8.4")
+                implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.4")
+                implementation("io.insert-koin:koin-compose:1.1.0")
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
+                implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.5.0")
                 implementation("androidx.datastore:datastore-preferences:1.0.0")
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
             }
         }
         val androidMain by getting {
             dependencies {
                 implementation("io.insert-koin:koin-android:3.5.3")
+                implementation("io.ktor:ktor-client-okhttp:2.3.7")
+            }
+        }
+
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+            dependencies {
+                implementation("io.ktor:ktor-client-darwin:2.3.7")
             }
         }
     }
@@ -44,4 +83,19 @@ android {
     compileSdk = 34
     defaultConfig { minSdk = 24 }
     compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
+}
+
+spotless {
+    kotlin {
+        target("src/**/*.kt")
+        ktlint("1.2.1").editorConfigOverride(
+            mapOf(
+                "max_line_length" to "off",
+                "ktlint_standard_filename" to "disabled",
+                "ktlint_standard_function-naming" to "disabled",
+                "ktlint_standard_no-wildcard-imports" to "disabled",
+                "ktlint_standard_property-naming" to "disabled"
+            )
+        )
+    }
 }
